4fb92f6b13fb107853855ede615481ce5d4c54a1
SkibaVA <49150472+SkibaVA@users.noreply.github.com>
2019-05-19 18:33:01 +0700

    Homework 08 Proxy. Доработка №2.

--- OtusJavaHW/hw08-proxy/src/main/java/ru/otus/handlers/InvocationHandlers.java
+++ OtusJavaHW/hw08-proxy/src/main/java/ru/otus/handlers/InvocationHandlers.java
@@ -2,46 +2,42 @@ package ru.otus.handlers;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+
+import ru.otus.annotations.Log;
 import ru.otus.interfaces.ICommander;
 import java.util.Arrays;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 final public class InvocationHandlers {
 	public static class TestLogging implements InvocationHandler 
 	{
 		
         private final ICommander myClass;
-        private Map<String, Method>  methodMap;
+        private Set<String> methodSignatureSet;
 
-        public TestLogging(ICommander myClass, Map<String, Method> methodMap) {
+        public TestLogging(ICommander myClass) {
             this.myClass = myClass;
-            this.methodMap = methodMap;
+            this.methodSignatureSet = new HashSet<String>();
+            
+            Class<?> clazz = this.myClass.getClass();
+            
+        	for(Method m: clazz.getMethods()) {
+        		if(m.isAnnotationPresent(Log.class)) {
+        			methodSignatureSet.add(getMethodSignature(m));
+        		}
+        	}
         }
 
+        private String getMethodSignature(Method m) {
+        	return m.getName() + "_" + Arrays.toString(m.getParameterTypes());
+        }
+        
 		@Override
 		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {	
 			
-			if(methodMap.containsKey(method.getName())) {
-				if(method.getParameterCount() == methodMap.get(method.getName()).getParameterCount()) {
-					
-					Class<?>[] paramLocalMethod = method.getParameterTypes();
-					Class<?>[] paramMapMethod = methodMap.get(method.getName()).getParameterTypes();
-					
-					boolean print = true;
-					
-					for(int i = 0; i < method.getParameterCount(); i++) {
-						if(paramLocalMethod[i].getName() != paramMapMethod[i].getName()) {
-							print = false;
-							break;
-						}
-					}
-					
-					if(print) {
-						System.out.println("Object '" + myClass.getClass().getName() + "', executed method: " + method.getName() + ", param: " + Arrays.toString(args));
-					}
-					
-				}
-				
+			if(methodSignatureSet.contains(getMethodSignature(method))) {
+				System.out.println("Object '" + myClass.getClass().getName() + "', executed method: " + method.getName() + ", param: " + Arrays.toString(args));
 			}
 			
 			return method.invoke(myClass, args);
--- OtusJavaHW/hw08-proxy/src/main/java/ru/otus/proxy/MyProxy.java
+++ OtusJavaHW/hw08-proxy/src/main/java/ru/otus/proxy/MyProxy.java
@@ -1,12 +1,8 @@
 package ru.otus.proxy;
 
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.HashMap;
-import java.util.Map;
 
-import ru.otus.annotations.Log;
 import ru.otus.classes.TestLoggingImpl;
 import ru.otus.handlers.InvocationHandlers;
 import ru.otus.interfaces.ICommander;
@@ -15,19 +11,13 @@ final public class MyProxy {
     static public ICommander TestLogging() {
     	
     	ICommander myClass = new TestLoggingImpl();
-    	Class<?> clazz = myClass.getClass();
-    	Class<?>[] interf =  clazz.getInterfaces();
     	
-    	Map<String, Method> methodMap = new HashMap<String, Method>();
-    	
-    	for(Method m: clazz.getMethods()) {
-    		if(m.isAnnotationPresent(Log.class)) {
-    			methodMap.put(m.getName(), m);
-    		}
-    	}
-    	
-        InvocationHandler handler = new InvocationHandlers.TestLogging(myClass, methodMap);
+        InvocationHandler handler = new InvocationHandlers.TestLogging(myClass);
         
-        return (ICommander) Proxy.newProxyInstance(myClass.getClass().getClassLoader(),interf, handler);
+        return (ICommander) Proxy.newProxyInstance(
+	        		myClass.getClass().getClassLoader()
+	        		,myClass.getClass().getInterfaces()
+	        		,handler
+        		);
     }
 }
